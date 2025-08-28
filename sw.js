8392fefe8513aa89cce6f114932991fbf2021625
tstrembu <81712317+tstrembu@users.noreply.github.com>
2025-08-27 22:53:33 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js (at /violin-positions-quest/sw.js)
-const CACHE = "vpq-v2"; // ← bump this string
+const CACHE = "vpq-v3"; // ← bump this string
 const ASSETS = [
     "./",                 // resolves to /violin-positions-quest/
   "./index.html",
